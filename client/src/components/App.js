c1c59af992a3bb597374caae74d2db4a763b393d
Jordon Smith <jsmith72@mail.uoguelph.ca>
2019-01-27 13:35:57 -0500

    Established connection between server and client entrypoints

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  // Render content to the screen
+  render() {
+    return (
+      <div>
+        <h1>Hello From The Front End</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create a sample root route
-app.get("/", (req, res) => {
+app.get("/api/matches", (req, res) => {
   res.send({ hello: "world" });
 });
 
